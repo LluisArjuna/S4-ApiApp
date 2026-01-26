417aafc55459afa4d5f961ad15be637e9f8c28db
LluisArjuna <lluisarjunav@gmail.com>
2026-01-26 13:31:28 +0100

    In the jokesUI file we will get the joke element paragraph where we will print the joke. We will get also the button that will execute the functions. The function showJoke recieves a Joke and its function is to print the joke in the jokeElement paragraph. We will get also the three buttons, each one with an addEventListener to to thefine the currentScore value. The showJoke will reset to null the currentScore value each time we execute the function.

---
+++ src/scripts/jokes/jokesUI.ts
@@ -0,0 +1,28 @@
+import type { joke } from "../interfaces";
+
+const jokeElement = document.getElementById('joke') as HTMLParagraphElement;
+export const nextButton = document.getElementById('next-joke') as HTMLButtonElement;
+
+export let currentScore: number | null = null;
+
+export function showJoke (joke:joke) {
+    try {
+        jokeElement.textContent = joke.joke;
+        currentScore = null;
+    } catch (error) {
+        console.error("Error: ",error);
+        jokeElement.textContent = 'Hi ha hagut un error carregant l’acudit.';
+    }
+};
+
+const score1 = document.getElementById('score_1') as HTMLButtonElement;
+const score2 = document.getElementById('score_2') as HTMLButtonElement;
+const score3 = document.getElementById('score_3') as HTMLButtonElement;
+
+score1.addEventListener('click', () => currentScore = 1);
+score2.addEventListener('click', () => currentScore = 2);
+score3.addEventListener('click', () => currentScore = 3);
+
+
+
+
