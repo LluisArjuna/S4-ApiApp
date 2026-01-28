4d220642536c705966eef8732851bf931c6cee4c
LluisArjuna <lluisarjunav@gmail.com>
2026-01-28 13:39:25 +0100

    We want to add a weather widget that shows weather information of the location where the web page is being initialized. The request will be inside a function because we'll need to pass the latitude and longitude params as well as a key.

--- src/scripts/apiRequests.ts
+++ src/scripts/apiRequests.ts
@@ -3,3 +3,16 @@ export const dadJokesRequest = new Request('https://icanhazdadjoke.com/', {
 });
 
 export const chuckNorrisRequest = new Request('https://api.chucknorris.io/jokes/random');
+
+
+
+export function weatherRequest(lat: number, lon: number) {
+  const apiKey = "ba38e628b053018df7e0bdf040e6bd26";
+
+  return new Request(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric&lang=ca`, {
+    headers: { "Accept": "application/json" }
+  });
+}
+
+
+
